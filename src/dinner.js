7740eab70a3bec8e6afc839e7c888c35c48c6d07
syednoman12 <syednoman199@gmail.com>
2020-06-25 14:57:00 -0700

    Create dinner.js

---
+++ src/dinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Dinner()
+{
+
+    return(
+
+        <div>
+            <h1>noman</h1>
+            <h1>shah</h1>
+        </div>
+    )
+}
+export default Dinner;
