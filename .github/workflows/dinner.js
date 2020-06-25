d6f4cbf89961d81f3cbda8dd55dca6fa4a46245a
syednoman12 <syednoman199@gmail.com>
2020-06-25 14:39:22 -0700

    Create dinner.js

---
+++ .github/workflows/dinner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Dinner()
+{
+
+    return(
+
+        <div>
+
+            <h1>today we have checken biryani</h1>
+            <h1>today we have checken karahi</h1>
+        </div>
+    )
+        
+    
+}
+export default Dinner;
